6544a7ad7d52e08e588767218baf2538a54ebdb5
Théo Laperrouse <theo.laperrouse@laposte.net>
2025-07-31 11:01:45 +0200

    fix: better font size calculation (#23)
    Co-authored-by: Théo Laperrouse <theo.laperrouse@energiency.com>

--- rollup.config.js
+++ rollup.config.js
@@ -11,50 +11,44 @@ const banner = `/*!
 const fileName = pkg.name.split('/').at(-1);
 
 module.exports = [
-  {
-    input: 'src/plugin.js',
-    output: ['.js', '.min.js'].map((suffix) => {
-      const config = {
-        name: 'ChartPieChartOutlabels',
-        file: `dist/${fileName}${suffix}`,
-        banner: banner,
-        format: 'umd',
-        indent: false,
-        plugins: [],
-        globals: {
-          'chart.js': 'Chart',
-          'chart.js/helpers': 'Chart.helpers'
-        }
-      };
+    {
+        input: 'src/plugin.js',
+        output: ['.js', '.min.js'].map((suffix) => {
+            const config = {
+                name: 'ChartPieChartOutlabels',
+                file: `dist/${fileName}${suffix}`,
+                banner: banner,
+                format: 'umd',
+                indent: false,
+                plugins: [],
+                globals: {
+                    'chart.js': 'Chart',
+                    'chart.js/helpers': 'Chart.helpers',
+                },
+            };
 
-      if (suffix.match(/\.min\.js$/)) {
-        config.plugins.push(
-          terser({
-            output: {
-              comments: /^!/
+            if (suffix.match(/\.min\.js$/)) {
+                config.plugins.push(
+                    terser({
+                        output: {
+                            comments: /^!/,
+                        },
+                    }),
+                );
             }
-          })
-        );
-      }
 
-      return config;
-    }),
-    external: [
-      'chart.js',
-      'chart.js/helpers',
-    ]
-  },
-  {
-    input: 'src/plugin.js',
-    output: {
-      file: pkg.module,
-      banner: banner,
-      format: 'esm',
-      indent: false
+            return config;
+        }),
+        external: ['chart.js', 'chart.js/helpers'],
     },
-    external: [
-      'chart.js',
-      'chart.js/helpers',
-    ]
-  },
+    {
+        input: 'src/plugin.js',
+        output: {
+            file: pkg.module,
+            banner: banner,
+            format: 'esm',
+            indent: false,
+        },
+        external: ['chart.js', 'chart.js/helpers'],
+    },
 ];
--- src/custom-helpers.js
+++ src/custom-helpers.js
@@ -1,72 +1,71 @@
 'use strict';
 
-import {Chart} from 'chart.js';
-import {valueOrDefault, isNullOrUndef, toLineHeight} from 'chart.js/helpers';
+import { Chart } from 'chart.js';
+import { valueOrDefault, isNullOrUndef, toLineHeight } from 'chart.js/helpers';
 
 function toFontString(font) {
-  if (!font || isNullOrUndef(font.size) || isNullOrUndef(font.family)) {
-    return null;
-  }
+    if (!font || isNullOrUndef(font.size) || isNullOrUndef(font.family)) {
+        return null;
+    }
 
-  return (font.style ? font.style + ' ' : '')
-    + (font.weight ? font.weight + ' ' : '')
-    + font.size + 'px '
-    + font.family;
+    return (
+        (font.style ? font.style + ' ' : '') + (font.weight ? font.weight + ' ' : '') + font.size + 'px ' + font.family
+    );
 }
 
 function textSize(ctx, lines, font) {
-  var items = [].concat(lines);
-  var ilen = items.length;
-  var prev = ctx.font;
-  var width = 0;
-  var i;
+    var items = [].concat(lines);
+    var ilen = items.length;
+    var prev = ctx.font;
+    var width = 0;
+    var i;
 
-  ctx.font = font.string;
+    ctx.font = font.string;
 
-  for (i = 0; i < ilen; ++i) {
-    width = Math.max(ctx.measureText(items[i]).width, width);
-  }
+    for (i = 0; i < ilen; ++i) {
+        width = Math.max(ctx.measureText(items[i]).width, width);
+    }
 
-  ctx.font = prev;
+    ctx.font = prev;
 
-  return {
-    height: ilen * font.lineHeight,
-    width: width
-  };
+    return {
+        height: ilen * font.lineHeight,
+        width,
+    };
 }
 
-function adaptTextSizeToHeight(height, minimum, maximum) {
-  var size = (height / 100) * 2.5;
-  if (minimum && size < minimum) {
-    return minimum;
-  }
-  if (maximum && size > maximum) {
-    return maximum;
-  }
-  return size;
+function adaptTextSizeToDimensions(width, height, minimum, maximum) {
+    const diag = Math.sqrt(Math.pow(width || 100, 2) + Math.pow(height || 100, 2));
+    const size = Math.round(diag * 0.02);
+
+    if (minimum && size < minimum) {
+        return minimum;
+    }
+    if (maximum && size > maximum) {
+        return maximum;
+    }
+    return size;
 }
 
-function parseFont(value, height) {
-  var size = valueOrDefault(value.size, Chart.defaults.defaultFontSize);
+function parseFont(value, width, height) {
+    var size = valueOrDefault(value.size, Chart.defaults.defaultFontSize);
 
-  if (value.resizable) {
-    size = adaptTextSizeToHeight(height, value.minSize, value.maxSize);
-  }
+    if (value.resizable) {
+        size = adaptTextSizeToDimensions(width, height, value.minSize, value.maxSize);
+    }
 
-  var font = {
-    family: valueOrDefault(value.family, Chart.defaults.defaultFontFamily),
-    lineHeight: toLineHeight(value.lineHeight, size),
-    size: size,
-    style: valueOrDefault(value.style, Chart.defaults.defaultFontStyle),
-    weight: valueOrDefault(value.weight, null),
-    string: ''
-  };
+    var font = {
+        family: valueOrDefault(value.family, Chart.defaults.defaultFontFamily),
+        lineHeight: toLineHeight(value.lineHeight, size),
+        lineWidth: toLineHeight(value.lineWidth, size),
+        size,
+        style: valueOrDefault(value.style, Chart.defaults.defaultFontStyle),
+        weight: valueOrDefault(value.weight, null),
+        string: '',
+    };
 
-  font.string = toFontString(font);
-  return font;
+    font.string = toFontString(font);
+    return font;
 }
 
-export {
-  textSize,
-  parseFont,
-};
+export { textSize, parseFont };
